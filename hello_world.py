bdd349b5be23a3e11e18f5c8a5a8cf9d3c893950
Enrique <enrique.ruvalcaba@iteso.mx>
2026-01-28 13:05:47 -0600

    Funcion de Hello World en Python

--- hello_world.py
+++ hello_world.py
@@ -6,6 +6,7 @@ Docstring for Analisis-Estatico.hello_world
 def saludo():
     """
     Docstring for saludo
+    Es una funcion que imprime el hellow world
     """
     print("Hola, como estas?")
 
